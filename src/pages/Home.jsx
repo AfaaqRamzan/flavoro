d453d26f52b64fbadf48f9e927ac912a26c62974
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-08-05 04:06:42 +0500

    Creating Cart Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import CategoryMenu from "../components/CategoryMenu";
 import Navbar from "../components/NavBar";
 import FoodItems from "../components/FoodItems";
+import Cart from "../components/Cart";
 const Home = () => {
   return (
     <>
       <Navbar />
       <CategoryMenu />
       <FoodItems />
+      <Cart />
  
     </>
   );
