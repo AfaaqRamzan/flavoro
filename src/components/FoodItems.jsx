47873396c26641ead6406e898a1d0a5c8b82e66a
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-08-04 22:38:41 +0500

    Creating Food-Card

---
+++ src/components/FoodItems.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FoodCard from './FoodCard'
+
+const FoodItems = () => {
+  return (
+    <div><FoodCard /></div>
+  )
+}
+
+export default FoodItems
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import CategoryMenu from "../components/CategoryMenu";
 import Navbar from "../components/NavBar";
+import FoodItems from "../components/FoodItems";
 const Home = () => {
   return (
     <>
       <Navbar />
       <CategoryMenu />
+      <FoodItems />
  
     </>
   );
