81c58b9285da2e1f51fe829db0560dd485987f3b
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-08-04 23:03:15 +0500

    Working with Food-Items Component

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
 
-const FoodCard = () => {
+const FoodCard = ({id, img, name, price, desc, rating}) => {
 
 
   return (
     <div className="font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2">
       <img
-        src="https://img.freepik.com/free-photo/seafood-pizza_74190-5944.jpg?w=996&t=st=1693062328~exp=1693062928~hmac=53fd9ad496580db41c6ca8066510cd89c6b0a0389de8bb6b875a78a1eda09cb5"
+        src={img}
         alt=""
         className="w-auto h-[130px]  hover:scale-110 cursor-grab transition-all duration-500 ease-in-out "
       />
       <div className="text-sm flex justify-between">
-        <h2>food name</h2>
-        <span className="text-green-500 ">Rs.200</span>
+        <h2>{name}</h2>
+        <span className="text-green-500 ">Rs.{price}</span>
       </div>
-      <p className="text-sm font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto sint soluta ipsam debitis praesentium sunt, beatae et. Repudiandae, modi quae!</p>
+      <p className="text-sm font-normal">{desc.slice(0, 50)} ...</p>
       <div className="flex justify-between ">
         <span className="flex justify-center items-center">
-          <AiFillStar className="mr-1 text-yellow-400" /> 4.5
+          <AiFillStar className="mr-1 text-yellow-400" /> {rating}
         </span>
         <button
           
