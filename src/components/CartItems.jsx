1f69061691b61c796f52bfc729656ae4a9ff14f5
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-08-05 17:24:55 +0500

    Deleting from the Cart

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../redux/slices/CartSlice";
 
-const CartItems = () => {
+const CartItems = ({id, name, img, qty, price}) => {
+  const dispatch = useDispatch()
+
   return (
     <div className="flex gap-2 shadow-md rounded-lg p-2 mb-3">
-      <MdDelete className="absolute right-7 text-gray-600 cursor-pointer" />
-      <img src="img" alt="" className="w-[50px] h-[50px] " />
+      <MdDelete 
+      onClick={()=>{
+        dispatch(removeFromCart({id, img, name, price, qty}))
+      }}
+      className="absolute right-7 text-gray-600 cursor-pointer" />
+      <img src={img} alt="" className="w-[50px] h-[50px] " />
       <div className="leading-5">
-        <h2 className="font-bold text-gray-800">name</h2>
+        <h2 className="font-bold text-gray-800">{name}</h2>
         <div className="flex justify-between ">
-          <span className="text-green-500 font-bold">Rs.200</span>
+          <span className="text-green-500 font-bold">Rs.{price}</span>
           <div className="flex justify-center items-center gap-2 absolute right-7">
             <AiOutlineMinus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all ease-linear cursor-pointer" />
-            <span>Qty</span>
+            <span>{qty}</span>
             <AiOutlinePlus className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all ease-linear cursor-pointer" />
           </div>
         </div>
