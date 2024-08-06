fec78cf10b2489bce0caf78bdaa77384e80a4bd9
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-08-06 19:11:06 +0500

    Adding Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Success from "./pages/Success";
 import Error from "./pages/Error";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -9,7 +10,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/success" element={<Success />} />
+          <Route
+            path="/success"
+            element={<ProtectedRoute element={<Success />} />}
+          />
           <Route path="/*" element={<Error />} />
         </Routes>
       </BrowserRouter>
