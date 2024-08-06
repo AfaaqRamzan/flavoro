db49f7749d8cf0fd4c0596d38998ea103fbf9a08
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-08-06 18:25:42 +0500

    Filtering data as per search

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
-
+import { setSearch } from "../redux/slices/SearchSlice";
+import { useDispatch } from "react-redux";
 
 const Navbar = () => {
-  
+  const dispatch = useDispatch();
+
   return (
     <nav className="flex flex-col lg:flex-row justify-between py-3 mx-6 mb-10">
       <div>
@@ -19,7 +20,7 @@ const Navbar = () => {
           id=""
           placeholder="Search here"
           autoComplete="off"
-          
+          onChange={(e) => dispatch(setSearch(e.target.value))}
           className="p-3 border border-gray-400 text-sm rounded-lg outline-none w-full lg:w-[25vw]"
         />
       </div>
